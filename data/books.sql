88cc8c57961d9da10264cc9ce8d5f465746297e3
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-04-15 01:41:49 +0300

    lab13

--- data/books.sql
+++ data/books.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS books;
-CREATE TABLE books (
+DROP TABLE IF EXISTS bookstable;
+-- DROP TABLE IF NOT EXISTS bookstable;
+CREATE TABLE bookstable (
   id SERIAL PRIMARY KEY,
-  author VARCHAR(255),
+  authors VARCHAR(255),
   title VARCHAR(255),
   isbn VARCHAR(255),
   image_url VARCHAR(255),
   description TEXT,
   bookshelf VARCHAR(255)
 )
-
--- server.js
+++ server.js
@@ -7,62 +7,65 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', (err) => console.log(err));
-//  static css file
 app.use(express.static('./public'));
-//  receve server data
 app.use(express.urlencoded({ extended: true }));
-// set the view engine
 app.set('view engine', 'ejs');
-// route 
+
 // app.get('/hello', (req, res) => {
 //     res.render('pages/index')
 // });
-app.get('/', (req, res) => {
-    let SQL = `SELECT * FROM booksTable;`
+
+app.get('/', getbook); 
+function getbook (req, res) {
+    let SQL = 'SELECT * FROM bookstable;'
     client
         .query(SQL)
         .then((data) => {
             console.log(data);
-            res.render('pages/index', { booksTable: data.rows});
+            res.render('pages/index', { booksnew: data.rows});
         })
         .catch((err) => {
             errorHandler(err, req, res);
         });
-});
+};
 app.post('/books', addTask);
 function addTask(req, res) {
     const { title,authors,isbn,image_url,description,bookshelf} = req.body;
-    const SQL =
-      'INSERT INTO booksTable (title,authors,isbn,image_url,description,bookshelf) VALUES ($1,$2,$3,$4,$5,$6);';
+    const SQL ='INSERT INTO bookstable (title,authors,isbn,image_url,description,bookshelf) VALUES ($1,$2,$3,$4,$5,$6);';
     const values = [title,authors,isbn,image_url,description,bookshelf];
-    client
-      .query(SQL, values)
+    
+    return client.query(SQL, values)
       .then(() => {
-        res.redirect('/books/:books_id');
+        res.redirect('/');
       })
       .catch((err) => {
         errorHandler(err, req, res);
       });
   }
-app.get('/books/:books_id', sssss);
-function sssss (req, res){
-    const SQL = 'SELECT * FROM booksTable WHERE id=$1;';
+app.get('/books/:books_id', setid);
+function setid (req, res){
+    const SQL = 'SELECT * FROM bookstable WHERE id=$1;';
     const values = [req.params.books_id];
     client
       .query(SQL, values)
       .then((results) => {
-        res.render('pages/books/detail', { book: results.rows[0] });
+        res.render('pages/books/detail', { booknew2: results.rows });
       })
       .catch((err) => {
         errorHandler(err, req, res);
       });
 }
+app.get('/books/add', details);
+function details (req, res){
+res.redirect('/');
+}
+
 app.get('/searches/new', newSearch);
 function newSearch(request, response) {
     response.render('pages/searches/new');
 }
-app.get('/searches', renderForm);
-app.post('/searches', findBook);
+app.get('/searches/show', renderForm);
+app.post('/searches/show', findBook);
 app.use('*', notFoundHandler);
 function renderForm(req, res) {
     res.render('pages/searches/new');
@@ -85,10 +88,10 @@ function findBook(req, res) {
         });
 }
 function Book(data) {
-    this.authors = (data.volumeInfo.authors && data.volumeInfo.authors[0]) || ' ';;
-    this.title = data.volumeInfo.title;
+    this.authors = (data.volumeInfo.authors && data.volumeInfo.authors[0]) || ' ';
+    this.title = data.volumeInfo.title ||' ';
     this.isbn = (data.volumeInfo.industryIdentifiers && data.volumeInfo.industryIdentifiers[0].identifier) || ' ';
-    this.img_url = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) || ' ';
+    this.image_url = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) || ' ';
     this.description = data.volumeInfo.description;
 }
 /////////////////////////////////////////////////////////
