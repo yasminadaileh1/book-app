01a8e3a4d9266fddd56f73d28bf5362182345729
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-05-01 17:57:14 +0300

    finished book app

--- server.js
+++ server.js
@@ -11,65 +11,20 @@ app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-// app.get('/hello', (req, res) => {
-//     res.render('pages/index')
-// });
 
-app.get('/', getbook); 
-function getbook (req, res) {
-    let SQL = 'SELECT * FROM bookstable;'
-    client
-        .query(SQL)
-        .then((data) => {
-            console.log(data);
-            res.render('pages/index', { booksnew: data.rows});
-        })
-        .catch((err) => {
-            errorHandler(err, req, res);
-        });
-};
-app.post('/books', addTask);
-function addTask(req, res) {
-    const { title,authors,isbn,image_url,description,bookshelf} = req.body;
-    const SQL ='INSERT INTO bookstable (title,authors,isbn,image_url,description,bookshelf) VALUES ($1,$2,$3,$4,$5,$6);';
-    const values = [title,authors,isbn,image_url,description,bookshelf];
-    
-    return client.query(SQL, values)
-      .then(() => {
-        res.redirect('/');
-      })
-      .catch((err) => {
-        errorHandler(err, req, res);
-      });
-  }
-app.get('/books/:books_id', setid);
-function setid (req, res){
-    const SQL = 'SELECT * FROM bookstable WHERE id=$1;';
-    const values = [req.params.books_id];
-    client
-      .query(SQL, values)
-      .then((results) => {
-        res.render('pages/books/detail', { booknew2: results.rows });
-      })
-      .catch((err) => {
-        errorHandler(err, req, res);
-      });
-}
-app.get('/books/add', details);
-function details (req, res){
-res.redirect('/');
-}
-
+// app.use('*', notFoundHandler);
 app.get('/searches/new', newSearch);
+app.post('/searches/show', findBook);
+app.post('/add', addBook);
+app.get('/', getbook);
+app.get('/detail/:id', detail);
+app.post('/delete/:id' , deleteBook)
+app.post('/update/:id' , updateBook)
+
 function newSearch(request, response) {
     response.render('pages/searches/new');
 }
-app.get('/searches/show', renderForm);
-app.post('/searches/show', findBook);
-app.use('*', notFoundHandler);
-function renderForm(req, res) {
-    res.render('pages/searches/new');
-}
+
 function findBook(req, res) {
     let url = `https://www.googleapis.com/books/v1/volumes?q=quilting`
     if (req.body.search === 'title') {
@@ -87,9 +42,77 @@ function findBook(req, res) {
             errorHandler(err, req, res);
         });
 }
+
+function addBook(req, res) {
+    const { title, authors, isbn, image_url, description, bookshelf } = req.body;
+    const SQL = 'INSERT INTO bookstable (title,authors,isbn,image_url,description,bookshelf) VALUES ($1,$2,$3,$4,$5,$6);';
+    const values = [title, authors, isbn, image_url, description, bookshelf];
+
+    return client.query(SQL, values)
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch((err) => {
+            errorHandler(err, req, res);
+        });
+}
+
+function getbook(req, res) {
+    let SQL = 'SELECT * FROM bookstable;'
+    client
+        .query(SQL)
+        .then((data) => {
+            console.log(data);
+            res.render('pages/index', { booksnew: data.rows });
+        })
+        .catch((err) => {
+            errorHandler(err, req, res);
+        });
+}
+
+function detail(req, res) {
+    const SQL = 'SELECT * FROM bookstable WHERE id=$1;';
+    const values = [req.params.id];
+    client
+        .query(SQL, values)
+        .then((results) => {
+            res.render('pages/books/detail', { booknew2: results.rows[0] });
+        })
+        .catch((err) => {
+            errorHandler(err, req, res);
+        });
+}
+function deleteBook (req , res){
+    let SQL = `DELETE FROM bookstable WHERE id=$1;`;
+    let value = [req.params.id];
+    client.query(SQL , value)
+    .then( ()=>{
+        res.redirect('/')
+    })
+}
+function updateBook(req , res){
+    let {title, authors, isbn, image_url, description, bookshelf } = req.body;
+    let SQL = `UPDATE bookstable SET title=$1, authors=$2, isbn=$3, image_url=$4, description=$5, bookshelf=$6 WHERE id=$7`
+    let id = req.params.id;
+    let values = [title, authors, isbn, image_url, description, bookshelf ,id];
+    client.query(SQL, values)
+    .then( ()=>{
+        res.redirect(`/detail/${id}`);
+    })
+}
+app.get('/books/add', details);
+function details(req, res) {
+    res.redirect('/');
+}
+
+
+
+
+
+
 function Book(data) {
     this.authors = (data.volumeInfo.authors && data.volumeInfo.authors[0]) || ' ';
-    this.title = data.volumeInfo.title ||' ';
+    this.title = data.volumeInfo.title || ' ';
     this.isbn = (data.volumeInfo.industryIdentifiers && data.volumeInfo.industryIdentifiers[0].identifier) || ' ';
     this.image_url = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) || ' ';
     this.description = data.volumeInfo.description;
